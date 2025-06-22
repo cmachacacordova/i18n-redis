6eed1ea04365119ac0cbfbb73118f29f4dec8aab
cmachacacordova <51836445+cmachacacordova@users.noreply.github.com>
2025-06-22 20:54:52 -0300

    Add export macro generation

--- include/i18n_redis.h
+++ include/i18n_redis.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "i18n_redis_export.h"
 #include <sw/redis++/redis++.h>
 #include <nlohmann/json.hpp>
 #include <string>
 
 namespace i18n_redis {
 
-void store_message(const std::string& key);
+I18N_REDIS_EXPORT void store_message(const std::string& key);
 
 }
