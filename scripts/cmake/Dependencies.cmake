a6120fb10090dea2e9e2948f2bbccb4ee564cc07
cmachacacordova <51836445+cmachacacordova@users.noreply.github.com>
2025-06-26 21:48:19 -0300

    Adjust triplets per preset

--- scripts/cmake/Dependencies.cmake
+++ scripts/cmake/Dependencies.cmake
@@ -2,8 +2,8 @@
 find_package(nlohmann_json CONFIG REQUIRED)
 find_package(redis++ CONFIG REQUIRED)
 
-# Dependencies used across build targets
-set(I18N_REDIS_DEPENDENCIES
+add_library(i18n-redis-deps INTERFACE)
+target_link_libraries(i18n-redis-deps INTERFACE
     $<IF:$<TARGET_EXISTS:redis++::redis++_static>,redis++::redis++_static,redis++::redis++>
     nlohmann_json::nlohmann_json
 )
