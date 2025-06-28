e309308643b15f38b0ad87f7afba274b180f67de
cmachacacordova <51836445+cmachacacordova@users.noreply.github.com>
2025-06-27 21:33:12 -0300

    Remove dependency interface target

--- scripts/cmake/Dependencies.cmake
+++ scripts/cmake/Dependencies.cmake
@@ -1,9 +1,9 @@
-# Locate third-party packages and set up interface target
+# Locate third-party packages and save dependency list
 find_package(nlohmann_json CONFIG REQUIRED)
 find_package(redis++ CONFIG REQUIRED)
 
-add_library(i18n-redis-deps INTERFACE)
-target_link_libraries(i18n-redis-deps INTERFACE
+# Dependencies used across build targets
+set(I18N_REDIS_DEPENDENCIES
     $<IF:$<TARGET_EXISTS:redis++::redis++_static>,redis++::redis++_static,redis++::redis++>
     nlohmann_json::nlohmann_json
 )
