aca67cceb756857e98eb7a048a81371c783d5d5d
cmachacacordova <51836445+cmachacacordova@users.noreply.github.com>
2025-06-28 22:00:32 -0300

    Update port to version 0.2.0

--- portfile.cmake
+++ portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL "https://github.com/cmachacacordova/i18n-redis.git"
-    REF be1d9a9623de827934f50bf260aab803a5f6d123 # tag v0.1.0
-    SHA512 36e085c078c45c3babd9edc1b3512d4025417cc737fbc2110dd15d73e8d7141e4d713fdda9643b40b3103e292321a39383a35ceed0175b910253be6e5f81ecf4
+    REF ee91ffb4ad42c1aad6e668494a4071023c2b39d7 # tag v0.2.0
+    SHA512 c09f205d87495ebe007ecb637a7c101b9bb1540c37d2f924a7f171c86847c553e64bbe7b557949942a4846839cdd3e7a0eb601be6786a9f51bb2209a78bad108
 )
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
